9bb76982c2bb9be6a86e87a7df32ff740811841c
manjeete <manjeetkr2017@gmail.com>
2024-01-02 19:26:52 +0530

    vercel does not allow file upload

--- src/pages/api/upload-file.js
+++ src/pages/api/upload-file.js
@@ -1,24 +1,31 @@
+// pages/api/upload.js
 import multer from 'multer';
-import fs from 'fs';
 import path from 'path';
+import fs from 'fs';
 
-const tmpUploadDir = '/tmp/uploads'; // Temporary storage directory
+// const uploadDir = './public/uploads'; // specify your upload directory
+const uploadDir = path.join(process.cwd(), 'public', 'uploads');
 
-// Ensure that the temporary upload directory exists
-if (!fs.existsSync(tmpUploadDir)) {
-  fs.mkdirSync(tmpUploadDir, { recursive: true });
-}
-
-const publicUploadDir = path.join(process.cwd(), 'public', 'uploads'); // Public upload directory
-
-// Ensure that the public upload directory exists
-if (!fs.existsSync(publicUploadDir)) {
-  fs.mkdirSync(publicUploadDir, { recursive: true });
+// Ensure that the upload directory exists
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir);
 }
 
 const allowedFileExtensions = ['.yaml', '.yml', '.txt'];
 
-const storage = multer.memoryStorage();
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, uploadDir);
+  },
+  filename: (req, file, cb) => {
+    const ext = path.extname(file.originalname);
+    if (allowedFileExtensions.includes(ext.toLowerCase())) {
+      cb(null, file.originalname);
+    } else {
+      cb(new Error('Invalid file type.'));
+    }
+  },
+});
 
 const uploadMiddleware = multer({
   storage,
@@ -30,11 +37,11 @@ const uploadMiddleware = multer({
       cb(new Error('Invalid file type.'));
     }
   },
-}).array('files', 5);
+}).array('files', 5); // 'files' is the field name for multiple files, 5 is the maximum number of files
 
 export const config = {
   api: {
-    bodyParser: false,
+    bodyParser: false, // Disabling built-in bodyParser
   },
 };
 
@@ -50,27 +57,9 @@ export default async function handler(req, res) {
         });
       });
 
-      // Move files from /tmp to public/uploads
-      await Promise.all(
-        req.files.map(async (file) => {
-          const ext = path.extname(file.originalname);
-          const fileName = file.originalname; // Keep the same file name
-          const tmpFilePath = path.join(tmpUploadDir, fileName);
-          const publicFilePath = path.join(publicUploadDir, fileName);
-
-          // Write file to public/uploads
-          fs.writeFileSync(publicFilePath, file.buffer);
-
-          // Unlink the file from the temporary directory
-          if (fs.existsSync(tmpFilePath)) {
-            fs.unlinkSync(tmpFilePath);
-          }
-        })
-      );
-
       res.status(200).json({ message: 'Files uploaded successfully.' });
     } catch (error) {
-      console.error(error);
+        console.log(error)
       res.status(500).json({ error: 'Error uploading files.' });
     }
   } else {
