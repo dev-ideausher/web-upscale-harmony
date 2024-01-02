c5f59275b6d02579cfce1542b8d920e67155f1b9
manjeete <manjeetkr2017@gmail.com>
2024-01-02 18:52:05 +0530

    relative path to absoulte path

--- src/pages/api/upload-file.js
+++ src/pages/api/upload-file.js
@@ -2,7 +2,7 @@
 import multer from 'multer';
 import path from 'path';
 import fs from 'fs';
-const path = require('path');
+// const path = require('path');
 const uploadDir = path.join(process.cwd(), 'public', 'uploads');
 
 // const uploadDir = './public/uploads'; // specify your upload directory
